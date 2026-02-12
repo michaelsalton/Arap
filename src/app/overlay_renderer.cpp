1be8c9c289086a0844d9ed76179baae7dc2248a7
michaelsalton <msalton6@gmail.com>
2026-02-12 16:18:35 -0500

    extracted colors

--- src/app/overlay_renderer.cpp
+++ src/app/overlay_renderer.cpp
@@ -1,13 +1,9 @@
 #include "overlay_renderer.h"
 #include "types/modes.h"
+#include "types/colors.h"
 #include <igl/per_vertex_normals.h>
 
-namespace {
-    const Eigen::RowVector3d kRed(1.0, 0.0, 0.0);
-    const Eigen::RowVector3d kGreen(0.0, 1.0, 0.0);
-    const Eigen::RowVector3d kOrange(1.0, 0.6, 0.0);
-    const Eigen::RowVector3d kDefaultFace(0.85, 0.85, 0.85);
-}
+using namespace colors;
 
 void OverlayRenderer::update(igl::opengl::ViewerData& data,
                              const Eigen::MatrixXd& V_current,
